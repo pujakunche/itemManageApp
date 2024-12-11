85f65528fe32dfb8fe60d4d56e2f1751ea66768b
pujakunche <kunchepuja1994@gmail.com>
2024-12-10 20:33:07 -0500

    first commit

---
+++ src/main/java/com/revature/itemManageApp/ItemManageAppApplication.java
@@ -0,0 +1,13 @@
+package com.revature.itemManageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemManageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemManageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/itemManageApp/ItemManageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.itemManageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemManageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
